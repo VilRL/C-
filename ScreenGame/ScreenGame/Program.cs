10c8adff82ba038acaa00f5d5c9f7470aa4b8656
Vitor Luiz Rodrigues de Lima <vitor02office@gmail.com>
2024-05-04 17:56:25 -0300

    Criado a primeira função de cadastro e aprimorado a entrada do usuario e exibição de titulo da opção

--- ScreenGame/ScreenGame/Program.cs
+++ ScreenGame/ScreenGame/Program.cs
@@ -1,43 +1,68 @@
 ﻿Dictionary<string, List<int>> colecaoDeJogos = new Dictionary<string, List<int>>();
 
 void ExibirOpções() 
-{ 
+{
+    Console.Clear();
     Console.WriteLine(@"
-        ░██████╗░░█████╗░███╗░░░███╗███████╗  ░█████╗░░█████╗░██╗░░░░░██╗░░░░░███████╗░█████╗░████████╗██╗░█████╗░███╗░░██╗
-        ██╔════╝░██╔══██╗████╗░████║██╔════╝  ██╔══██╗██╔══██╗██║░░░░░██║░░░░░██╔════╝██╔══██╗╚══██╔══╝██║██╔══██╗████╗░██║
-        ██║░░██╗░███████║██╔████╔██║█████╗░░  ██║░░╚═╝██║░░██║██║░░░░░██║░░░░░█████╗░░██║░░╚═╝░░░██║░░░██║██║░░██║██╔██╗██║
-        ██║░░╚██╗██╔══██║██║╚██╔╝██║██╔══╝░░  ██║░░██╗██║░░██║██║░░░░░██║░░░░░██╔══╝░░██║░░██╗░░░██║░░░██║██║░░██║██║╚████║
-        ╚██████╔╝██║░░██║██║░╚═╝░██║███████╗  ╚█████╔╝╚█████╔╝███████╗███████╗███████╗╚█████╔╝░░░██║░░░██║╚█████╔╝██║░╚███║
-        ░╚═════╝░╚═╝░░╚═╝╚═╝░░░░░╚═╝╚══════╝  ░╚════╝░░╚════╝░╚══════╝╚══════╝╚══════╝░╚════╝░░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░╚══╝");
+░██████╗░░█████╗░███╗░░░███╗███████╗  ░█████╗░░█████╗░██╗░░░░░██╗░░░░░███████╗░█████╗░████████╗██╗░█████╗░███╗░░██╗
+██╔════╝░██╔══██╗████╗░████║██╔════╝  ██╔══██╗██╔══██╗██║░░░░░██║░░░░░██╔════╝██╔══██╗╚══██╔══╝██║██╔══██╗████╗░██║
+██║░░██╗░███████║██╔████╔██║█████╗░░  ██║░░╚═╝██║░░██║██║░░░░░██║░░░░░█████╗░░██║░░╚═╝░░░██║░░░██║██║░░██║██╔██╗██║
+██║░░╚██╗██╔══██║██║╚██╔╝██║██╔══╝░░  ██║░░██╗██║░░██║██║░░░░░██║░░░░░██╔══╝░░██║░░██╗░░░██║░░░██║██║░░██║██║╚████║
+╚██████╔╝██║░░██║██║░╚═╝░██║███████╗  ╚█████╔╝╚█████╔╝███████╗███████╗███████╗╚█████╔╝░░░██║░░░██║╚█████╔╝██║░╚███║
+░╚═════╝░╚═╝░░╚═╝╚═╝░░░░░╚═╝╚══════╝  ░╚════╝░░╚════╝░╚══════╝╚══════╝╚══════╝░╚════╝░░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░╚══╝");
 
     Console.WriteLine("\nDigite 1 Para Cadastrar Um Jogo");
     Console.WriteLine("Digite 2 Para Exibir Todos Os Jogos");
     Console.WriteLine("Digite 3 Para Avaliar Um Jogo");
     Console.WriteLine("Digite 4 Para Exibir A Nota Média De Um Jogo");
+    Console.WriteLine("Digite 0 Para Sair\n");
 
-    Console.Write("Digte uma opção: ");
+    Console.Write("Digite uma opção: ");
     int opcaoDigitada = int.Parse(Console.ReadLine());
 
-    switch (opcaoDigitada)
+    if (opcaoDigitada < 4)
     {
-        case 1:
-            Console.WriteLine("1");
-            break;
-        case 2:
-            Console.WriteLine("2");
-            break;
-        case 3:
-            Console.WriteLine("3");
-            break;
-        case 4: 
-            Console.WriteLine("4");
-            break;
+        switch (opcaoDigitada)
+        {
+            case 1:
+                cadastrarJogo();
+                break;
+            case 2:
+                Console.WriteLine("2");
+                break;
+            case 3:
+                Console.WriteLine("3");
+                break;
+            case 4:
+                Console.WriteLine("4");
+                break;
+        }
+    }else{
+        Console.WriteLine("\nVocê digitou uma opção invalida!");
+        ExibirOpções();
     }
+
+}
+
+void exibirTitulo(string titulo) 
+{
+    Console.Clear();
+    int quantidadeDeLetras = titulo.Length;
+    string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
+    Console.WriteLine(asteriscos);
+    Console.WriteLine(titulo);
+    Console.WriteLine($"{asteriscos}\n");
 }
 
 void cadastrarJogo() 
 {
-
+    exibirTitulo("CADASTRAR JOGO");
+    Console.Write("Digite o nome do jogo: ");
+    string jogoDigitado = Console.ReadLine();
+    colecaoDeJogos.Add(jogoDigitado, new List<int>());
+    Console.WriteLine($"\nO jogo {jogoDigitado} foi cadastrado com sucesso!!!");
+    Thread.Sleep(3000);
+    ExibirOpções();
 }
 
 void exibirJogos() 
--- ScreenGame/ScreenGame/obj/Debug/net8.0/ScreenGame.AssemblyInfo.cs
+++ ScreenGame/ScreenGame/obj/Debug/net8.0/ScreenGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18b1dcce1a049ab07aef65037a6c2277e8ae09c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
